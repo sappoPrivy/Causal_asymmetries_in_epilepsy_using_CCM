8c9dc3281db563abcf11f54e144ae20fde22e552
Choedon <choedon@kth.se>
2025-02-28 15:37:17 +0100

    Meeting 1

---
+++ main.py
@@ -0,0 +1,16 @@
+import os
+import mne
+import numpy
+import matplotlib
+
+file = 'chb01_03.edf'
+raw_edf = mne.io.read_raw_edf(file, preload = True)
+
+print(raw_edf.info)
+
+events = mne.find_events(raw_edf, stim_channel="F7-T7", output='onset')
+
+data = raw_edf.get_data()
+print(data)
+
+raw_edf.plot(block=True, scalings=dict(eeg=10e-5), duration=40)
